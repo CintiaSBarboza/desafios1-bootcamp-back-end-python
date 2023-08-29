b88af14f5ec177d6530a3d9dd80899b2265b04d5
Cíntia da Silva Barboza <138408241+CintiaSBarboza@users.noreply.github.com>
2023-08-29 18:56:05 -0300

    Add files via upload

--- Exercicios/exercicios_listas_tuplas_dicionarios.py
+++ Exercicios/exercicios_listas_tuplas_dicionarios.py
@@ -1,17 +1,17 @@
 #1) Faça um programa que peça as quatro notas de 10 alunos, calcule e armazene numa lista a
 #média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
 
-nota
-aluno1 = float(input('Digite a nota do aluno 1: '))
-aluno2 = float(input('Digite a nota do aluno 2: '))
-aluno3 = float(input('Digite a nota do aluno 3: '))
-aluno4 = float(input('Digite a nota do aluno 4: '))
-aluno5 = float(input('Digite a nota do aluno 5: '))
-aluno6 = float(input('Digite a nota do aluno 6: '))
-aluno7 = float(input('Digite a nota do aluno 7: '))
-aluno8 = float(input('Digite a nota do aluno 8: '))
-aluno9 = float(input('Digite a nota do aluno 9: '))
-aluno10 = float(input('Digite a nota do aluno 10: '))
+nota1aluno1 = float(input('Digite a nota1 do aluno 1: '))
+nota1aluno2 = float(input('Digite a nota1 do aluno 2: '))
+nota1aluno3 = float(input('Digite a nota1 do aluno 3: '))
+nota1aluno4 = float(input('Digite a nota1 do aluno 4: '))
+nota1aluno5 = float(input('Digite a nota1 do aluno 5: '))
+nota1aluno6 = float(input('Digite a nota1 do aluno 6: '))
+nota1aluno7 = float(input('Digite a nota1 do aluno 7: '))
+nota1aluno8 = float(input('Digite a nota1 do aluno 8: '))
+nota1aluno9 = float(input('Digite a nota1 do aluno 9: '))
+nota1aluno10 = float(input('Digite a nota1 do aluno 10: '))
+
 
 #2) Programa nome ao contrário em maiúsculas. Faça um programa que permita ao usuário digitar 
 # o seu nome e em seguida mostre o nome do usuário de trás para frente utilizando somente letras maiúsculas.
@@ -34,11 +34,11 @@ dicionario = {}
 # Seapessoaresponderpositivamente a 2 questões ela deve ser classificada com "Suspeita", entre
 # 3 e 4 como "Cúmplice" e 5 "Assassino ". Caso contrário, ele será classificado como "Inocente."
 
-pergunta1 = input('Telefonou para a vítima? Digite: S-Sim ou N-Não ')
-pergunta2 = input('Esteve no local do crime? Digite: S-Sim ou N-Não ')
-pergunta3 = input('Mora perto da vítima? Digite: S-Sim ou N-Não ')   
-pergunta4 = input('Devia para a vítima? Digite: S-Sim ou N-Não ')
-pergunta5 = input('Já trabalhou com a vítima? Digite: S-Sim ou N-Não ')
+pergunta1 = int(input('Telefonou para a vítima? Digite: 1-Sim ou 0-Não '))
+pergunta2 = int(input('Esteve no local do crime? Digite: 1-Sim ou 0-Não '))
+pergunta3 = int(input('Mora perto da vítima? Digite: 1-Sim ou 0-Não '))   
+pergunta4 = int(input('Devia para a vítima? Digite: 1-Sim ou 0-Não '))
+pergunta5 = int(input('Já trabalhou com a vítima? Digite: 1-Sim ou 0-Não '))
 
 somaRespostas = pergunta1 + pergunta2 + pergunta3 + pergunta4 + pergunta5
 
@@ -46,7 +46,7 @@ if somaRespostas<2:
     print('Inocente')
 elif somaRespostas == 2:
     print('Suspeita')
-elif (3 <= somaRespostas <=4):
-    print('Cúmplice')
-else:
+elif somaRespostas == 5:
     print('Assassino')
+else:
+    print('Cúmplice')
